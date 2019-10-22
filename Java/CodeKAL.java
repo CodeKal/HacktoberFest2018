179a5a5154c186401a5e252eec3d3871b6452ac0
Gokceduzyol <32934286+Gokceduzyol@users.noreply.github.com>
2019-10-22 18:23:59 +0300

    Update CodeKAL.java

--- Java/CodeKAL.java
+++ Java/CodeKAL.java
@@ -7,6 +7,9 @@ public class CodeKal {
 
 		System.out.println("CodeKAL'ı çok seviyorum.");
 		System.out.println("CodeKAL'da olduğum için çok mutluyum.");
+		
+		System.out.println("I love CodeKAL!");
+		System.out.println("I am very happy to be a part of CodeKAL.");
 
 	}
 
